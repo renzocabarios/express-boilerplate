e77b89ffd6f5595795bab2c40101032ddd125176
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-01-09 21:21:10 +0800

    [MOD] default mongo con

--- app/env/index.js
+++ app/env/index.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 const ENV = {
   PORT: process.env.PORT || 9000,
-  MONGO_CON: process.env.MONGO_CON || "mongodb://localhost/main",
+  MONGO_CON:
+    process.env.MONGO_CON ||
+    "mongodb://localhost:27017/sample?directConnection=true",
   HASH_SALT: Number(process.env.HASH_SALT) || 10,
   JWT_KEY: process.env.JWT_KEY || "secret",
 };
