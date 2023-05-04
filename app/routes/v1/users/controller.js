973a017ba5e31c1d4230dc4f3386d52363e2dad9
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-05-05 03:37:53 +0800

    Add generateAccess in users controller

--- app/routes/v1/users/controller.js
+++ app/routes/v1/users/controller.js
@@ -1,13 +1,20 @@
 import service from "./service.js";
 import bcrypt from "bcrypt";
 import ENV from "../../../env/index.js";
-import { transaction } from "../../../utils/index.js";
+import { transaction, generateAccess } from "../../../utils/index.js";
 import mongoose from "mongoose";
 import auth from "../auth/service.js";
 
 const getAll = async (_req, _res) => {
   const data = await service.getAll();
-  _res.send({ data, status: "success", message: "Get user success" });
+  _res.send({
+    data,
+    status: "success",
+    message: "Get user success",
+    meta: {
+      access: generateAccess({}),
+    },
+  });
 };
 
 const add = async (_req, _res) => {
@@ -18,7 +25,8 @@ const add = async (_req, _res) => {
     await transaction(
       session,
       async () => {
-        await auth.add({ email, password: hashed }, session);
+        const temp = await service.add({ ...res }, session);
+        await auth.add({ user: temp[0]._id, email, password: hashed }, session);
         return await service.add({ ...res }, session);
       },
       "Create user"
